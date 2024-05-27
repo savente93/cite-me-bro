82614221c270c0d7630c380b7390e719c091153e
Sam Vente <savente93@proton.me>
2024-05-27 18:25:13 +0200

    add more bib entryies and formatted citations to compare against

--- src/styles/ieee.rs
+++ src/styles/ieee.rs
@@ -81,6 +81,34 @@ mod test {
     use super::*;
     use crate::parsing::entry::EntryType;
     use anyhow::Result;
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref FORMATTED_CITATIONS: BTreeMap<&'static str, &'static str> = {
+            let mut m = BTreeMap::new();
+            m.insert("breiman2001random","L. Breiman, \"Random forests,\" Machine learning, vol. 45, no. 1, pp. 5–32, 2001. doi: https://doi.org/10.1023/a:1010933404324.");
+            m.insert("10.1093/femsec/fiw174", "J. Liao, X. Cao, L. Zhao, et al., \"The importance of neutral and niche processes for bacterial community assembly differs between habitat generalists and specialists,\" FEMS Microbiology Ecology, vol. 92, no. 11, fiw174, Aug. 2016, issn: 0168-6496. doi:10.1093/femsec/fiw174. [Online]. Available: https://doi.org/10.1093/femsec/fiw174.");
+            m.insert("article", "P. J. Cohen, \"The independence of the continuum hypothesis,\" Proceedings of the National Academy of Sciences, vol. 50, no. 6, pp. 1143–1148, 1963.");
+            m.insert("book", "L. Susskind and G. Hrabovsky, Classical mechanics: the theoretical minimum. New York, NY: Penguin Random House, 2014.");
+            m.insert("booklet", "M. Swetla, Canoe tours in Sweden, Distributed at the Stockholm Tourist Office, Jul. 2015.");
+            m.insert("inbook", "L. A. Urry, M. L. Cain, S. A. Wasserman, P. V. Minorsky, and J. B. Reece, \"Photosynthesis,\" in Campbell Biology. New York, NY: Pearson, 2016, pp. 187–221.");
+            m.insert("incollection", "H. M. Shapiro, \"Flow cytometry: The glass is half full,\" in Flow Cytometry Protocols, T. S. Hawley and R. G. Hawley, Eds., New York, NY: Springer, 2018, pp. 1–10.");
+            m.insert("inproceedings", "P. Holleis, M. Wagner, and J. Koolwaaij, \"Studying mobile context-aware social services in the wild,\" in Proc. of the 6th Nordic Conf. on Human-Computer Interaction, ser. NordiCHI, New York, NY: ACM, 2010, pp. 207–216.");
+            m.insert("manual", "R Core Team, R: A language and environment for statistical computing, R Foundation for Statistical Computing, Vienna, Austria, 2018.");
+            m.insert("mastersthesis", "J. Tang, \"Spin structure of the nucleon in the asymptotic limit,\" M.S. thesis, Massachusetts Institute of Technology, Cambridge, MA, Sep. 1996.");
+            m.insert("misc", "NASA, Pluto: The 'other' red planet, https://www.nasa.gov/nh/pluto- the-other-red-planet, Accessed: 2018-12-06, 2015.");
+            m.insert("phdthesis", "R. C. Rempel, \"Relaxation effects for coupled nuclear spins,\" Ph.D. dissertation, Stanford University, Stanford, CA, Jun. 1956.");
+            m.insert("proceedings", "S. Stepney and S. Verlan, Eds., Proceedings of the 17th International Conference on Computation and Natural Computation, Fontainebleau, France, vol. 10867, Lecture Notes in Computer Science, Cham, Switzerland: Springer, 2018");
+            m.insert("techreport", "V. Bennett, K. Bowman, and S. Wright, \"Wasatch Solar Project final report,\" Salt Lake City Corporation, Salt Lake City, UT, Tech. Rep. DOE- SLC-6903-1, Sep. 2018.");
+            m.insert(
+                "unpublished",
+                "M. Suresh, \"Evolution: A revised theory,\" 2006",
+            );
+
+            m
+        };
+    }
+
     #[test]
     fn single_author_fmt() -> Result<()> {
         let author = OwnedFullName {
